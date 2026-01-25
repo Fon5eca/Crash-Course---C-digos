43800744f73507299bbcffc4b2af2decfd70508f
Fon5eca <fonseblad@gmail.com>
2026-01-25 17:27:32 -0600

    Actualizaciones al Alien Invasion

--- invasion espacial/alien_invasion.py
+++ invasion espacial/alien_invasion.py
@@ -5,9 +5,10 @@ from ship import Ship
 class GameSettings():
     def __init__(self):
         '''Create the game settings.'''
-        self.width = 1600
-        self.height = 900
+        self.width = 1000
+        self.height = 800
         self.screen_color = (75, 34, 171)     #Screen colors in RGB
+        self.ship_speed = 0.5
 
 
 class AlienInvasion():
@@ -15,6 +16,7 @@ class AlienInvasion():
         '''Cuando se crea una instancia (partida), que se guarden estos ajustes y se inicialice pygame.'''
         pygame.init()
         self.settings = GameSettings()
+        self.ship_speed = self.settings.ship_speed
         self.screen = pygame.display.set_mode((self.settings.width, self.settings.height))     #Creates a window
         pygame.display.set_caption("Alien Invasion")     #Name of the window
         self.ship = Ship(self)     #Esto se hace para dar acceso de una clase a otra y viceversa.
@@ -22,19 +24,38 @@ class AlienInvasion():
     def run_game(self):
         '''Iniciar el juego'''
         while True:
-            # Siempre esté viendo las acciones con el teclado
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
+            self.ship.update()
 
-            #Actualizar el color de fondo
-            self.screen.fill(self.settings.screen_color)
-            
-            self.ship.blitme()
+    def _check_events(self):
+        #Siempre esté viendo las acciones con el teclado
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            #Actualizar el frame (para los fps)
-            pygame.display.flip()
+            #Movernos a la derecha e izquierda
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
 
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+    
+    def _update_screen(self):
+        #Actualizar el color de fondo
+        self.screen.fill(self.settings.screen_color)
+        
+        #Pegar la imagen en la pestaña
+        self.ship.blitme()
+
+        #Actualizar el frame (para los fps)
+        pygame.display.flip()
 
 if __name__ == '__main__':
     partida = AlienInvasion()
--- invasion espacial/ship.py
+++ invasion espacial/ship.py
@@ -6,8 +6,21 @@ class Ship():
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.image = pygame.image.load("/home/fon5eca/Programación/CrashCourse/invasion espacial/Images/AI_Ship.png")
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect()     #ESto da las propiedades de la imagen, convirtiéndola a rectángulo
         self.rect.midbottom = self.screen_rect.midbottom
+        self.moving_right = False
+        self.moving_left = False
+        self.ship_speed = ai_game.ship_speed
+        self.x = float(self.rect.x)     #HAbilitarla para poder sumarle numeros decimales
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+    
+    def update(self):
+        if self.moving_right:
+            self.x += self.ship_speed     #Aumenta en 1 la posición en x de la nave (ahora es un rectángulo)
+
+        if self.moving_left:
+            self.x -= self.ship_speed
+        
+        self.rect.x = self.x
